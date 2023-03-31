fa1ee82cd59c6496ae1acfba96b8435f89fa2fef
roistaff <roistaff1983@gmail.com>
2023-03-31 22:45:24 +0900

    new commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/gin-gonic/gin"
+)
+type INFO struct {
+	Total uint64 `json:"total"`
+	Free uint64 `json:"free"`
+	Use uint64 `json:"use"`
+}
+func info_get() INFO {
+	v, _ := mem.VirtualMemory()
+	info := INFO{
+		Total: v.Total /1024 /1024,
+		Free:  v.Free /1024 /1024,
+		Use:   v.Used /1024 /1024,
+	}
+	return info
+}
+func main() {
+	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        c.Next()
+    })
+	r.GET("/info", func(c *gin.Context) {
+		info := info_get()
+		c.JSON(200,info)
+	})
+	r.Run(":8080")
+}
+
